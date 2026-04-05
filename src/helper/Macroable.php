3b9091e5a670a746696c9686a6d9339ba2458baf
silveridc <admin@silveridc.cn>
2026-04-06 01:11:48 +0800

    Update Macroable.php

--- src/helper/Macroable.php
+++ src/helper/Macroable.php
@@ -8,7 +8,7 @@
 // +----------------------------------------------------------------------
 // | Author: yunwuxin <448901948@qq.com>
 // +----------------------------------------------------------------------
-namespace zhaoyj_helper;
+namespace helper;
 
 use Closure;
 use think\exception\FuncNotFoundException;
