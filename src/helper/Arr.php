60f1a683d3201908f3a607ad212acd0f1d76cff3
silveridc <admin@silveridc.cn>
2026-04-06 01:11:10 +0800

    Update Arr.php

--- src/helper/Arr.php
+++ src/helper/Arr.php
@@ -9,7 +9,7 @@
 // | Author: yunwuxin <448901948@qq.com>
 // +----------------------------------------------------------------------
 
-namespace zhaoyj_helper;
+namespace helper\contract;
 
 use ArrayAccess;
 use InvalidArgumentException;
