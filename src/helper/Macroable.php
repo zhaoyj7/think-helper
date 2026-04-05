0ced8e7a8080ca9648d79aeec6b74be478cfb3c6
silveridc <admin@silveridc.cn>
2026-04-06 00:55:40 +0800

    修改namespace

--- src/helper/Macroable.php
+++ src/helper/Macroable.php
@@ -8,7 +8,7 @@
 // +----------------------------------------------------------------------
 // | Author: yunwuxin <448901948@qq.com>
 // +----------------------------------------------------------------------
-namespace think\helper;
+namespace zhaoyj_helper;
 
 use Closure;
 use think\exception\FuncNotFoundException;
