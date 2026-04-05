ed73310745256311e81bb995f94c106c1a0eaa8f
silveridc <admin@silveridc.cn>
2026-04-06 00:56:04 +0800

    修改namespace

--- src/Collection.php
+++ src/Collection.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 declare (strict_types = 1);
 
-namespace think;
+namespace zhaoyj_helper;
 
 use ArrayAccess;
 use ArrayIterator;
