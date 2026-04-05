8c09ae4e0ddd30a9b283de6b0031257b2edb943b
silveridc <admin@silveridc.cn>
2026-04-06 00:55:22 +0800

    修改namespace

--- src/helper/Arr.php
+++ src/helper/Arr.php
@@ -9,7 +9,7 @@
 // | Author: yunwuxin <448901948@qq.com>
 // +----------------------------------------------------------------------
 
-namespace think\helper;
+namespace zhaoyj_helper;
 
 use ArrayAccess;
 use InvalidArgumentException;
